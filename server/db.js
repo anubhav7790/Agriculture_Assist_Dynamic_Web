a2b50f18115e2a0ebfa1bcf35b0c68ee602df27a
Anubhav Kumar <anubhavkumar779@gmail.com>
2026-08-19 19:58:19 +0530

    Add SSL support for MySQL connection

--- server/db.js
+++ server/db.js
@@ -3,12 +3,17 @@ import mysql from "mysql2/promise";
 
 dotenv.config();
 
+const sslCa = process.env.MYSQL_SSL_CA_BASE64
+  ? Buffer.from(process.env.MYSQL_SSL_CA_BASE64, "base64").toString("utf8")
+  : process.env.MYSQL_SSL_CA;
+
 export const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || "localhost",
   port: Number(process.env.MYSQL_PORT || 3306),
   user: process.env.MYSQL_USER || "root",
   password: process.env.MYSQL_PASSWORD || "",
   database: process.env.MYSQL_DATABASE || "krishi_vikas",
+  ssl: sslCa ? { ca: sslCa, rejectUnauthorized: true } : undefined,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
